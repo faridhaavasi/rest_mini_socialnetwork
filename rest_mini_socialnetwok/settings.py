39de65d602b1e55d52300c71e9387288d82e7e6f
faridhaavasi <faridhavasi777@gmail.com>
2024-02-25 18:37:14 +0330

    set db sqlite3

--- rest_mini_socialnetwok/settings.py
+++ rest_mini_socialnetwok/settings.py
@@ -76,16 +76,26 @@ WSGI_APPLICATION = 'rest_mini_socialnetwok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'USER': 'test',
+#         'PASSWORD': 'test',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#         'NAME': db',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'db',
-        'PORT': '5432',
-        'NAME': 'shopin_db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 
 # Password validation
